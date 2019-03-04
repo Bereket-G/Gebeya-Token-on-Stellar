45ca81310daad5ac3476525f8267b6b053d59619
Mikiyas Amdu <mikias.amdu@gmail.com>
2019-03-04 15:02:22 +0300

    fix issue with crypto

---
+++ src/app/create/create.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as StellarSdk from 'stellar-sdk';
+
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+
+@Component({
+  selector: 'app-create',
+  templateUrl: 'create.page.html',
+  styleUrls: ['create.page.scss']
+})
+export class CreatePage implements OnInit {
+  
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
